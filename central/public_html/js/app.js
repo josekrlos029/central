33b5e95860b3f3c5d0a5b93871835fb0a231ad11
Jose Carlos Jimenez Montenegro <josekrlos029@hotmail.com>
2014-11-18 21:13:14 -0500

    ....

--- central/public_html/js/app.js
+++ central/public_html/js/app.js
@@ -52,6 +52,7 @@ function listaMensajeros(){
     
     $.ajax({
         type: "POST",
+        async: false,
         url: url,
         data: data
     })
@@ -179,7 +180,21 @@ function popEntregado2(idServicio) {
 
 function popAsignarDomicilio(idDomicilio) {
     $("#idDomi").val(idDomicilio);
+    var $this = $(this),
+            theme = $this.jqmData("theme") || $.mobile.loader.prototype.options.theme,
+            msgText = $this.jqmData("msgtext") || $.mobile.loader.prototype.options.text,
+            textVisible = $this.jqmData("textvisible") || $.mobile.loader.prototype.options.textVisible,
+            textonly = !!$this.jqmData("textonly");
+    html = $this.jqmData("html") || "";
+    $.mobile.loading("show", {
+        text: msgText,
+        textVisible: textVisible,
+        theme: theme,
+        textonly: textonly,
+        html: html
+    });
     listaMensajeros();
+    $.mobile.loading("hide");
 }
 
 function popAsignarServicio(idServicio) {
